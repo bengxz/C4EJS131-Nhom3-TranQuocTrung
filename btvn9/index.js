e3241ac29df78a844caa2ee94ab5bf1cac3f1dfa
bengxz <102458543+bengxz@users.noreply.github.com>
2022-08-23 16:55:00 +0700

    Add files via upload

---
+++ btvn9/index.js
@@ -0,0 +1,32 @@
+let arrA = [5,4,7,8,10,20,1];
+arrA.sort(function(a,b){
+    return a-b;
+})
+console.log(arrA)
+
+let arrB = [1,2,'a',{}, 'b', 'c', 2,4,5];
+let arrC = []
+for (let i=0; i<arrB.length; i++){
+    if (typeof arrB[i] === "number"){
+        arrC.push(arrB[i])
+    }
+}
+console.log (arrC)
+
+let arrD =[1,4,10,100,50,69, 96]
+arrD.sort(function(a,b){
+    return a-b;
+})
+console.log('So be nhat trong mang la:',arrD[0]);
+console.log('So lon nhat trong mang la:',arrD[(arrD.length)-1]);
+
+let arrE = [1,2,'a', 'b', 'c', 2,4,5];
+for (let i=1; i<=3;i++)
+arrE.unshift(arrE.pop());
+console.log(arrE)
+
+let arrF = [1,23,41,5,6,1,14,66,1124,1,1,55,15,0]
+let index = arrF.findIndex(
+    (arrF, index) => arrF === 1 && index > 9
+);
+console.log(index);
